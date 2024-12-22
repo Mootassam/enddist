f943d01b9b98c4f91f61d0470f0818c5a08114bb
Mootassam <mootassame@gmail.com>
2024-12-22 17:13:57 -0500

    add the new dist

---
+++ assets/Nodata-6f20316e.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-8b8b7948.js";function e(){return n.jsxs("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",padding:20,fontSize:15,flexDirection:"column"},children:[n.jsx("img",{src:"/icons/nofound.svg"}),n.jsx("span",{style:{color:"#8C8C8C",fontWeight:500},children:" There's no transactions till now! "})," "]})}export{e as N};
---
+++ assets/Search-207c139e.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8b8b7948.js";function a(){return e.jsx("div",{className:"search__page",children:e.jsx("div",{className:"search__input",children:e.jsx("input",{type:"text",placeholder:"search"})})})}export{a as default};
